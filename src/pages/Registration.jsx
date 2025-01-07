37f57f6616040c81415180bf59aceeb44e51b2f8
Redoan Hasan <redoanhasan57@gmail.com>
2025-01-07 07:41:09 +0600

    login page completed except logout

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,6 +1,19 @@
-import { Link } from "react-router"
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router"
+import { AuthContext } from "../provider/AuthProvider"
 
 const Registration = () => {
+    const {signInWithGoogle,createUser,updateUserProfile} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleGoogleSignIn = async() => {
+        try {
+            await signInWithGoogle();
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
       <div className='flex justify-center items-center min-h-[calc(100vh-306px)]'>
         <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl '>
@@ -17,7 +30,7 @@ const Registration = () => {
               Get Your Free Account Now.
             </p>
   
-            <div className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
+            <div onClick={handleGoogleSignIn} className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
               <div className='px-4 py-2'>
                 <svg className='w-6 h-6' viewBox='0 0 40 40'>
                   <path
