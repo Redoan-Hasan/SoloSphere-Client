a0c1363eb75c9d89a2288d4f62144aca0d6dad0b
Redoan Hasan <redoanhasan57@gmail.com>
2025-03-01 06:53:22 +0600

    39.29

--- src/components/TabCategories.jsx
+++ src/components/TabCategories.jsx
@@ -1,7 +1,17 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import JobCard from "./JobCard";
+import { useEffect, useState } from "react";
 const TabCategories = () => {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      await fetch(`${import.meta.env.VITE_API_URL}/jobs`)
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+    };
+    getData();
+  }, []);
   return (
     <div className="my-10">
       <Tabs>
@@ -18,13 +28,28 @@ const TabCategories = () => {
         </div>
 
         <TabPanel>
-          <h2><JobCard /></h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {jobs.filter((job) => job.category === "Web Development")
+            .map((job) => (
+              <JobCard key={job.id} job={job} />
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Graphics Design</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {jobs.filter((job) => job.category === "Graphics Design")
+            .map((job) => (
+              <JobCard key={job.id} job={job} />
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Digital Marketing</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {jobs.filter((job) => job.category === "Digital Marketing")
+            .map((job) => (
+              <JobCard key={job.id} job={job} />
+            ))}
+          </div>
         </TabPanel>
       </Tabs>
     </div>
