2819c0bd525234756f3e1dea4c9ed993650d725b
Redoan Hasan <redoanhasan57@gmail.com>
2025-03-28 04:34:18 +0600

    axios interceptors response handling

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -6,6 +8,23 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    const {logOut}=useAuth();
+    const navigate = useNavigate();
+    axiosSecure.interceptors.response.use(res =>{
+        console.log("dekhtechi je intercept kore kina , korle eita console e print hbe fr");
+        // console.log(res);
+        return res;
+    },
+    async error =>{
+        console.log("error from axios interceptor",error);
+        if(error.response.status === 401 || error.response.status === 403){
+            await logOut();
+            navigate("/login");
+        }
+        return Promise.reject(error);
+    }
+)
+
     return axiosSecure;
 };
 
