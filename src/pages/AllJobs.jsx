6f2521936f48dc6b41333112c4068f59708ac0d3
Redoan Hasan <redoanhasan57@gmail.com>
2025-04-08 22:14:15 +0600

    pagination fix for search functionality

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -9,6 +9,7 @@ const AllJobs = () => {
   const [filter, setFilter] = useState("");
   const [sort, setSort] = useState("");
   const [search, setSearch] = useState("");
+  const [searchText, setSearchText] = useState("");
   const [jobs, setJobs] = useState([]);
   console.log(search);
   useEffect(() => {
@@ -26,12 +27,14 @@ const AllJobs = () => {
   useEffect(() => {
     const getCount = async () => {
       const { data } = await axios.get(
-        `${import.meta.env.VITE_API_URL}/allJobsCount?filter=${filter}`
+        `${
+          import.meta.env.VITE_API_URL
+        }/allJobsCount?filter=${filter}&search=${search}`
       );
       setCount(data.count);
     };
     getCount();
-  }, [filter]);
+  }, [filter, search]);
   // console.log(filter);
   const numberOfPages = Math.ceil(count / itemsPerPage);
   const pages = [...Array(numberOfPages).keys()].map((element) => element + 1);
@@ -44,8 +47,15 @@ const AllJobs = () => {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    const text = e.target.search.value;
-    setSearch(text);
+    setSearch(searchText);
+    setCurrentPage(1);
+  };
+
+  const handleReset = () => {
+    setFilter("");
+    setSearch("");
+    setSort("");
+    setSearchText("");
   };
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
@@ -72,6 +82,8 @@ const AllJobs = () => {
           <form onSubmit={handleSearch}>
             <div className="flex p-1 overflow-hidden border rounded-lg focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
               <input
+                onChange={(e)=>setSearchText(e.target.value)}
+                value={searchText}
                 className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
                 type="text"
                 name="search"
@@ -98,7 +110,7 @@ const AllJobs = () => {
               <option value="asc">Ascending Order</option>
             </select>
           </div>
-          <button onClick={() => setCurrentPage(1)} className="btn">
+          <button onClick={ handleReset} className="btn">
             Reset
           </button>
         </div>
